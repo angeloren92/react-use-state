d56a5d9d3ae9255e5242f25ee096a105126d27eb
angeloren92 <angeloren92@gmail.com>
2026-03-11 19:10:55 +0100

    finalizing logic

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -2,19 +2,31 @@ import { useState } from "react"
 
 export default function AppMain({ languages }) {
 
-  const [count, setCount] = useState(0)
+  let [count, setCount] = useState(0)
+  const markup = languages.find(element => element.id === count)
 
   return (
     <main>
       <div className="container">
         <div className="d-flex" >
           {
-            languages.map(element => (
-              <button key={element.id} onClick={ () => setCount(count +1) }>{element.title}</button>
-            ))}
+            languages.map(({ id, title }) => (
+              <div className="card">
+                <button key={id} onClick={() => setCount(count = id)}>
+                  {title}
+                </button>
+              </div>
+            ))
+          }
         </div>
+        {markup && (
+          <div className="card-body">
+            <h3>{markup.title}</h3>
+            <p>{markup.description}</p>
+          </div>
+        )
+        }
       </div>
-      <p>{count}</p>
     </main >
 
   )
