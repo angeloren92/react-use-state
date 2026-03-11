9f75c827fd221d9c647ebf5bb814fe701d26eed9
angeloren92 <angeloren92@gmail.com>
2026-03-11 18:15:58 +0100

    reset logic

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -1,21 +1,20 @@
-import AccortionBody from "./AccortionBody.jsx"
+import { useState } from "react"
 
 export default function AppMain({ languages }) {
 
-  return (
+  const [count, setCount] = useState(0)
 
+  return (
     <main>
       <div className="container">
-        <div className="row row-cols-1">
-          <AccortionBody languages={languages} />
-          {languages.map(({ id, description }) => (
-            <div key={id} className="col-12">
-              {description}
-            </div>
-          ))
-          }
+        <div className="d-flex" >
+          {
+            languages.map(element => (
+              <button key={element.id} onClick={ () => setCount(count +1) }>{element.title}</button>
+            ))}
         </div>
       </div>
+      <p>{count}</p>
     </main >
 
   )
